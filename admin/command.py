4e77460fd961ef02497a88b3f04a64c2ced252fc
shashah <shashaminchok@gmail.com>
2024-02-18 22:53:30 +0800

    fix view avail seats bug

--- admin/command.py
+++ admin/command.py
@@ -110,12 +110,13 @@ def is_valid_date_format(date_str: str) -> bool:
         input_date = datetime.strptime(date_str, "%Y-%m-%d")
     except ValueError:
         return False
+    return True
 
 def is_valid_date_range(date_str: str, data) -> bool:
     today = datetime.now().date()
     max_date = today + timedelta(days=7)
     input_date = datetime.strptime(date_str, "%Y-%m-%d").date()
-    return today <= input_date <= max_date and date_str in data["dates "]
+    return today <= input_date <= max_date and date_str in data["dates"]
 
 async def date(update: Update, context: CallbackContext) -> int:
     date_str = update.message.text
